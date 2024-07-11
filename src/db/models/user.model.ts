ea199f9c974e979f1cd5255c8165cfe030b212ce
Precious Nnam <nnamvannessa@gmail.com>
2024-07-11 23:05:40 +0100

    feat:creating models

---
+++ src/db/models/user.model.ts
@@ -0,0 +1,66 @@
+import {BelongsToMany, Column, DataType, HasMany, Model,Scopes, Table} from "sequelize-typescript";
+import { RefreshToken } from "./refresh-token.model";
+import { DocumentUser } from "./document-user.model";
+import { UserRole } from "./user-role.model";
+import { Role } from "./role.model";
+
+
+@Scopes(()=> ({
+    withRoles:{
+        include:[{
+            model: UserRole,
+            attributes:['createdAt', 'updatedAt'],
+            include:[Role]
+        }]
+    }
+}))
+
+
+
+@Table({tableName: 'user', underscored: true}) 
+class User extends Model{
+    @Column(DataType.STRING)
+    email!: string;
+
+
+    @Column(DataType.STRING)
+    password!: string;
+
+    @Column(DataType.BOOLEAN)
+    isVerified!: boolean;
+
+    @Column(DataType.STRING)
+    verificationToken!: string;
+
+    @Column(DataType.STRING)
+    passwordResetToken!: string;
+
+    @HasMany(()=>RefreshToken,{
+        onDelete: 'CASCADE'
+    })
+      
+    refreshTokens!: Array<RefreshToken>
+
+
+    @BelongsToMany(()=>Role,{
+        through:{
+            model:()=> UserRole
+        }
+    })
+    roles!: Array<Role>
+
+    @HasMany(()=> UserRole,{
+        onDelete: 'CASCADE'
+    })
+
+    userRoles!: Array<UserRole>
+
+    @HasMany(()=> DocumentUser,{
+            onDelete: 'CASCADE'
+        })
+
+    sharedDocuments!: Array<DocumentUser>
+
+}
+  
+export {User};
