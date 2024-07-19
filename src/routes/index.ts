22b85aae383aa2fffc48ce3939cfd9ba1e92a515
Precious Nnam <nnamvannessa@gmail.com>
2024-07-20 00:55:19 +0100

    feat: document routes including share document functionality

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import user from "./user.route";
 import auth from "./auth.route";
+import document from "./document.route";
 
 const router = Router();
 
 router.use("/user", user);
 router.use("/auth", auth);
+router.use("/document", document);
 
 export default router;
