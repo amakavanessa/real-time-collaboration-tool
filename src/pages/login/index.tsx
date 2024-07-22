37df300e62cc7184c338f5c5a3a6b5230f3b55f8
Precious Nnam <nnamvannessa@gmail.com>
2024-07-22 23:50:22 +0100

    feat:front-end application set up, tailwind configuration

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="w-full flex-col sm:justify-center items-center p-6 sm:pb-96 bg-gray-100 dark:bg-slate-900 text-primary">
+      I am a login page
+    </div>
+  );
+};
+
+export default Login;
